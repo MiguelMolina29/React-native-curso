baefd33650bf95f8f24793096098a87f006bdf0f
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-12-05 18:11:47 -0600

    Objetos-literales

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import TiposBasicos from './typescript/TiposBasicos'
+// import TiposBasicos from './typescript/TiposBasicos'
+import ObjetosLiterales from './typescript/ObjetosLiterales'
 
 const App = () => {
   return (
     <div className="mt-2">
       <h1>Introduccion a TS - react</h1>
       <hr />
-      <TiposBasicos />
+      {/* <TiposBasicos /> */}
+      <ObjetosLiterales/>
     </div>
   )
 }

c51703dbef6ab16023c395832f7a1b26575eadd9
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-12-06 17:38:19 -0600

    Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 // import TiposBasicos from './typescript/TiposBasicos'
 // import ObjetosLiterales from './typescript/ObjetosLiterales'
-import Funciones from './typescript/Funciones'
+// import Funciones from './typescript/Funciones'
+import Contador from './components/Contador';
 
 const App = () => {
   return (
@@ -9,7 +10,9 @@ const App = () => {
       <h1>Introduccion a TS - react</h1>
       <hr />
       {/* <TiposBasicos /> */}
-      <Funciones/>
+      {/* <Funciones/> */}
+      <Contador />
+
     </div>
   )
 }

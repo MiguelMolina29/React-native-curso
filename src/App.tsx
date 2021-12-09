e3ab187375e0a23dada2bd9b365cfa766a92a75a
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-12-08 18:10:00 -0600

    Custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 // import TiposBasicos from './typescript/TiposBasicos'
 // import ObjetosLiterales from './typescript/ObjetosLiterales'
 // import Funciones from './typescript/Funciones'
-import Contador from './components/Contador';
+// import Contador from './components/Contador';
+import ContadorConHook from './components/ContadorConHook'
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <hr />
       {/* <TiposBasicos /> */}
       {/* <Funciones/> */}
-      <Contador />
+      <ContadorConHook />
 
     </div>
   )
